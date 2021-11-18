761c2feace1992886651b57004ecbf631ca2cdf8
Animesh-03 <adithaanimesh76@gmail.com>
2021-11-18 19:21:23 +0530

    add employee management

--- demo/src/main/java/com/example/demo/ParkingSlotRepository.java
+++ demo/src/main/java/com/example/demo/ParkingSlotRepository.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface ParkingSlotRepository extends CrudRepository<ParkingSlot,Long>
--- src/Components/ManageEmployees.js
+++ src/Components/ManageEmployees.js
@@ -1,7 +1,55 @@
+import axios from 'axios';
+import {useEffect, useState} from 'react'
+
 const ManageEmployees = () => {
+
+    const [employees,setEmployees] = useState();
+
+    const [firstName,setFirstName] = useState("");
+    const [lastName,setLastName] = useState("");
+    const [email,setEmail] = useState("");
+    const [phoneNumber,setPhoneNumber] = useState("");
+    const [assignedTo,setAssignedTo] = useState("");
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+
+    const handleAddEmployee = (e) => {
+        e.preventDefault();
+        
+
+        axios.post("http://localhost:8080/employee/add",null, {params:{
+            "firstName":firstName,
+            "lastName":lastName,
+            "email":email,
+            "phoneNumber":phoneNumber,
+            "assignedTo":assignedTo,
+            "userName":username,
+            "password":password
+        }}).then(res => {
+            console.log(res);
+        })
+    }
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/employee/all").then((res) => setEmployees(res.data));
+    },[])
+
+
+
     return (
         <div className="ManageEmployees">
-            <h2>Employees</h2>
+            <h1>Add Employee </h1>
+            <input id="employee-first-name" placeholder="Enter first name" onChange={e => setFirstName(e.target.value)} />
+            <input id="employee-last-name" placeholder="Enter last name" onChange={e => setLastName(e.target.value)} />
+            <input id="employee-email" placeholder="Enter email" onChange={e => setEmail(e.target.value)} />
+            <input id="employee-phone-number" placeholder="Enter phone number" onChange={e => setPhoneNumber(e.target.value)} />
+            <input id="employee-assigned-to" placeholder="Enter assigned to" onChange={e => setAssignedTo(e.target.value)} />
+            <input id="employee-username" placeholder="Enter username" onChange={e => setUsername(e.target.value)} />
+            <input id="employee-password" placeholder="Enter password" onChange={e => setPassword(e.target.value)} />
+            <button id="employee-add-btn" onClick={handleAddEmployee} >Add</button>
+
+            <h2>Employee List</h2>
+            <p>{JSON.stringify(employees)}</p>
         </div>  
      );
 }
