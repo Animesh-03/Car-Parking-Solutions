b9ae812f7b8240fa3f1f7639967d59f4b2748d5a
Animesh-03 <adithaanimesh76@gmail.com>
2021-11-28 17:33:55 +0530

    add filter slots by isBooked

--- demo/src/main/java/com/example/demo/ParkingSlotRepository.java
+++ demo/src/main/java/com/example/demo/ParkingSlotRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ParkingSlotRepository extends CrudRepository<ParkingSlot,Long>
 {
-    @Query(value = "SELECT * FROM cps.parking_slot s WHERE s.location_id = :locationId",nativeQuery = true)
-    // @Query("select id, x, y from test where x = :x", nativeQuery = true)
+    @Query(value = "SELECT * FROM cps.parking_slot s WHERE s.location_id = :locationId and is_booked=false",nativeQuery = true)
+    // @Query("select id, x, y from test where x = :x and is_booked=false", nativeQuery = true)
     public Iterable<ParkingSlot> findAllByLocationId(Long locationId);
 }
